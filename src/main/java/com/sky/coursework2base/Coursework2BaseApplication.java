76f3ace711152ddb632a6c5d6e66b66c5256aa5e
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-06-02 13:15:01 +0300

    initial

---
+++ src/main/java/com/sky/coursework2base/Coursework2BaseApplication.java
@@ -0,0 +1,13 @@
+package com.sky.coursework2base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework2BaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sky/coursework2base/Coursework2BaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.coursework2base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2BaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
